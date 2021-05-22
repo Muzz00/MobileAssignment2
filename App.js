d70a5447999df8ee509820e07b6f07567e340125
Muzz00 <57882850+Muzz00@users.noreply.github.com>
2021-05-22 15:35:39 +1200

    GPS working with MapView

---
+++ App.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Button, Text } from 'react-native';
+// import MapView from 'react-native-maps'
+import MapView, { Marker, Callout } from 'react-native-maps'
+import * as Location from 'expo-location';
+
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: -32.88083800623871,
+      longitude: 172.70746492207425,
+    }
+  }
+
+  componentDidMount() {
+    // this._getLocation();
+    console.log("hello")
+    this.updateLocation();
+    this.forceUpdate();
+  }
+
+
+
+
+  updateLocation = () => {
+    this.setState({
+      latitude: -36.88083800623871,
+      longitude: 174.70746492207425
+    })
+  }
+
+  getLocation = async () =>{
+
+    let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+    let location = await Location.getCurrentPositionAsync({});
+    console.log(location)
+    console.log(location.coords.longitude)
+    this.setState({ latitude: location.coords.latitude, longitude: location.coords.longitude })
+  }
+  
+  render() {
+    console.log('sfad');
+    let latitude = 33.7872131;
+    let longitude = -84.381931;
+
+    if (this.state.latitude != 0) {
+      console.log(this.state.latitude)
+      latitude = this.state.latitude;
+      longitude = this.state.longitude;
+    }
+
+    
+
+    return (
+      <>
+      <MapView
+        style={{ ...StyleSheet.absoluteFillObject }}
+        initialRegion={{
+          // latitude: 33.7872131,
+          // longitude: -84.381931,
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: .005,
+          longitudeDelta: .005
+        }} region={{latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: .005,
+          longitudeDelta: .005}} >
+        
+        <Marker
+          // coordinate={{ latitude: 33.7872131, longitude: -84.381931 }}
+          coordinate={{ latitude: latitude, longitude: longitude }}
+          title='Flatiron School Atlanta'
+          description='This is where the magic happens!'
+        ></Marker>
+
+      </MapView>
+      <Text>HellowWorld</Text>
+      <Button onPress={this.getLocation} title="Hello" />
+      </>
+    );
+  }
+}
