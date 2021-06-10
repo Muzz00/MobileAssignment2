b8c408ff0c2caa7769a84d49743d26317d4b0b94
Muzz00 <57882850+Muzz00@users.noreply.github.com>
2021-06-10 18:14:25 +1200

    Clean up code

--- android/app/src/main/java/com/maps/MainApplication.java
+++ android/app/src/main/java/com/maps/MainApplication.java
@@ -6,11 +6,6 @@ import android.net.Uri;
 
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.github.reactnativecommunity.location.RNLocationPackage;
-import com.github.reactnativecommunity.location.RNLocationPackage;
-import com.github.reactnativecommunity.location.RNLocationPackage;
-import com.mustansirzia.fused.FusedLocationPackage;
-import com.mustansirzia.fused.FusedLocationPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
